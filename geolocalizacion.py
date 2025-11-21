817aa1a175738873e9a01fb18e2143611ff08bb0
Angie Diaz <adiaz@sistecredito.com>
2025-11-21 17:58:10 -0500

    feat: agregar modulo de validacion DIVIPOLA

--- geolocalizacion.py
+++ geolocalizacion.py
@@ -1,14 +1,20 @@
+import json
 import requests
 import pandas as pd
+
+from utils_divipola import (
+    cargar_divipola,
+    buscar_en_divipola,
+    RUTA_DIVIPOLA
+)
 
 API_KEY = "AIzaSyBep8722UIKODElaxjOCPXjlUr85zS8rzI" 
 BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 
-
 LATITUD = 8.882597
 LONGITUD = -75.797453
 
-def obtener_departamento_de_api(lat: float, lon: float) -> str | None:
+def obtener_departamento_y_municipio_de_api(lat: float, lon: float) -> tuple[str  | None, str | None]:
 
     params = {
         'latlng': f"{lat},{lon}",
@@ -26,11 +32,14 @@ def obtener_departamento_de_api(lat: float, lon: float) -> str | None:
         
         if data.get('status') == 'OK' and data.get('results'):
             address_components = data['results'][0].get('address_components', [])
+
             for component in address_components:
                 types = component.get('types', [])
                 long_name = component.get('long_name')
+
                 if not long_name:
                     continue
+
 
                 if 'administrative_area_level_1' in types:
                     nombre_departamento = long_name
@@ -47,12 +56,18 @@ def obtener_departamento_de_api(lat: float, lon: float) -> str | None:
                     return nombre_departamento, nombre_municipio
                 
         print("   ❌ Error: La API no pudo encontrar el departamento o el JSON no tiene el formato esperado.")
-        return None
+        return None, None
         
     except requests.exceptions.RequestException as e:
         print(f"   ❌ Error al conectar con la API: {e}")
-        return None
+        return None, None
     
 if __name__ == "__main__":
     
-    departamento = obtener_departamento_de_api(LATITUD, LONGITUD)
+    departamento, municipio = obtener_departamento_y_municipio_de_api(LATITUD, LONGITUD)
+
+    df_divipola = cargar_divipola(RUTA_DIVIPOLA)
+    print("\n Buscando coincidencia en DIVIPOLA..")
+    info_divipola = buscar_en_divipola(df_divipola, departamento, municipio)
+    print(f"   ✅ Resultado en DIVIPOLA:")
+    print(json.dumps(info_divipola, indent=4, ensure_ascii=False))
---
+++ utils_divipola.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import unicodedata
+
+RUTA_DIVIPOLA = "data/DIVIPOLA.xlsx"
+
+def normalizar_texto(texto: str) -> str:
+   
+    if not isinstance(texto, str):
+        return ""
+    nfkd = unicodedata.normalize("NFKD", texto)
+    solo_ascii = "".join([c for c in nfkd if not unicodedata.combining(c)])
+    return solo_ascii.upper().strip()
+
+def cargar_divipola(ruta: str) -> pd.DataFrame:
+    
+    df = pd.read_excel(ruta)
+
+    df["Departamento_Normalizado"] = df["Nombre Departamento"].apply(normalizar_texto)
+    df["Municipio_Normalizado"] = df["Nombre Municipio"].apply(normalizar_texto)
+
+    df["Latitud_num"] = pd.to_numeric(df["Latitud"], errors="coerce")
+    df["Longitud_num"] = pd.to_numeric(df["longitud"], errors="coerce")
+
+    return df
+
+
+def buscar_en_divipola(df_divipola: pd.DataFrame,
+                       depto_google: str,
+                       muni_google: str) -> dict | None:
+    
+    dep_norm = normalizar_texto(depto_google)
+    muni_norm = normalizar_texto(muni_google)
+
+    coincidencias = df_divipola[
+        (df_divipola["Departamento_Normalizado"] == dep_norm) &
+        (df_divipola["Municipio_Normalizado"] == muni_norm)
+    ]
+
+    if coincidencias.empty:
+        print("No se encontró coincidencia en DIVIPOLA para:",
+              depto_google, "/", muni_google)
+        return None
+
+    fila = coincidencias.iloc[0]
+
+
+    return {
+        "codigo_departamento": int(fila["Código Departamento"]),
+        "nombre_departamento": fila["Nombre Departamento"],
+        "codigo_municipio": int(fila["Código Municipio"]),
+        "nombre_municipio": fila["Nombre Municipio"],
+        "latitud_divipola": fila["Latitud_num"],
+        "longitud_divipola": fila["Longitud_num"],
+    }
+
+
+
+
+
+
+
+
